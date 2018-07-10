b0fb3999cd458b8ca7d3dd9b9ee1fd390a79a133
alex.fan <alex.fan@okchem.com>
2018-07-10 17:55:49 +0800

    tmp commit

--- src/js/creational/CircleGenerator.js
+++ src/js/creational/CircleGenerator.js
@@ -5,18 +5,75 @@
             return this.replace(
                 /{([^{}]*)}/g,
                 function (a, b) {
-                    var r = o[b] || '&nbsp;';
-                    return typeof r === 'string' || typeof r === 'number' ? r : a;
+                    var r = o[b] || '&nbsp;'
+                    return typeof r === 'string' || typeof r === 'number' ? r : a
                 }
             );
         };
     }
 
     var _tplCanvas = [
-        '<canvas id="tzCanvas"></canvas>'
+        '<canvas id="{id}" width="{width}" height="{height}">',
+        '</canvas>'
     ].join('')
 
+    var CircleFactory = function () {
+        this.types = {}
+        this.create = function (type) {
+
+        }
+        this.register = function (type, cls) {
+            if (cls.prototype.init && cls.prototype.init) {
+                this.types[type] = cls
+            }
+        }
+    }
+
+    var CircleGenerator = (function () {
+        var instance
+
+        var init = function () {
+            var _cf = new CircleFactory()
+
+            var create = function (config) {
+                _cf.create(config)
+            }
+
+            return {
+                create: create
+            }
+        }
+
+        var getInstance = function () {
+            if (!instance) {
+                instance = init()
+            }
+            return instance
+        }
+
+        return {
+            getInstance: getInstance
+        }
+    })()
+
     $(win.document).ready(function () {
-        $(win.document.body).append(_tplCanvas)
+        var width = 600, height = 400
+
+        $(win.document.body).append(_tplCanvas.supplant({
+            id: 'myCanvas',
+            width: width,
+            height: height
+        }))
+
+        var canvas = $('#myCanvas')
+        var ctx = canvas.getContext("2d")
+
+        canvas.click(function (e) {
+            CircleGenerator.getInstance().create({
+                x: e.pageX,
+                y: e.pageY,
+                ctx: ctx
+            })
+        })
     })
 })(window, jQuery)
